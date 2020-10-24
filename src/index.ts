c4c8f934a684da806ef6f513d87cc63a9d79de51
Steve Mao <maochenyan@gmail.com>
2020-10-24 23:35:59 +1100

    add string

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import * as group from './Array'
 import * as json from './json'
 import * as record from './Record'
+import * as string from './String'
 
 export {group}
 export {json}
 export {record}
+export {string}
