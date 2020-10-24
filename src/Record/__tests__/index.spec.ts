333244d1def0909f9a2bcad7d33909e38bbc3f81
Steve Mao <maochenyan@gmail.com>
2020-10-24 23:29:28 +1100

    add record

---
+++ src/Record/__tests__/index.spec.ts
@@ -0,0 +1,35 @@
+import union from '../'
+
+describe('union', () => {
+    test('should successfully union all keys of all records', () => {
+        const reality = union([{
+            foo: 'foo',
+        }, {
+            bar: 'bar'
+        }])
+        const expected = {
+            foo: 'foo',
+            bar: 'bar'
+        }
+        expect(reality).toStrictEqual(expected);
+    });    
+
+    test('the latter one should overwrite the previous one', () => {
+        const reality = union([{
+            foo: 'foo',
+        }, {
+            bar: 'bar'
+        }, {
+            baz: 'baz'
+        }, {
+            foo: 'foo2'
+        }])
+        const expected = {
+            foo: 'foo2',
+            bar: 'bar',
+            baz: 'baz'
+        }
+
+        expect(reality).toStrictEqual(expected);
+    });    
+})
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import * as group from './Array'
 import * as json from './json'
+import * as record from './Record'
 
 export {group}
 export {json}
+export {record}
