07033be729f0df324e1f588169fa3d8cfcd4c3f1
Steve Mao <maochenyan@gmail.com>
2020-10-25 00:02:36 +1100

    add heterogeneousSequence

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,11 @@ import * as json from "./json";
 import * as record from "./Record";
 import * as string from "./String";
 import * as decode from "./decode";
+import * as heterogeneousSequence from "./heterogeneousSequence";
 
 export { group };
 export { json };
 export { record };
 export { string };
 export { decode };
+export { heterogeneousSequence };
