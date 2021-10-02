185742c25c0bc5ad0c0156f1d3ba1319020d5d6b
Steve Mao <maochenyan@gmail.com>
2021-10-02 20:07:50 +1000

    add more args

--- src/function/index.ts
+++ src/function/index.ts
@@ -9,7 +9,8 @@ export function memPipe<A, B, C>(a: A, ab: (a: A) => B, bc: (b: B) => C): C;
 export function memPipe<A, B, C>(
   a: IO<A>,
   b?: (a: IO<A>) => IO<B>,
-  c?: (b: IO<B>) => IO<C>
+  c?: (b: IO<B>) => IO<C>,
+  d?: (c: IO<C>) => IO<D>,
 ): unknown {
   switch (arguments.length) {
     case 1:
@@ -18,6 +19,8 @@ export function memPipe<A, B, C>(
       return pipe(a, b!, mem);
     case 3:
       return pipe(a, b!, c!, mem);
+    case 4:
+      return pipe(a, b!, c!, d!, mem);
   }
   return;
 }
